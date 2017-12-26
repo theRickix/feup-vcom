2319193fd43142a80fdbcc2d3b7bdda70fe34747
Unknown <mario.shadic.esteves@hotmail.com>
2017-12-26 23:32:11 +0000

    training program is now able to go through files in a given directory alphabetically

--- TP2/src/TrainingProgram.py
+++ TP2/src/TrainingProgram.py
@@ -1,31 +1,43 @@
 import cv2
-from os import walk
 import ObjectDefinitions
+import os
 from tkinter import filedialog
 from tkinter import *
+from os import *
+
+# Training program to recognize various object from the AID (Aerial Image Database)
 
 # Create window to get the directory where the images for training are
-pathSelectionWindow = Tk()
 directory = filedialog.askdirectory()
+# used to obtain all files and directories in a given root directory
 pathSelectionInfo = walk(directory)
-pathSelectionWindow.destroy()
 
 for root, dirs, files in pathSelectionInfo:
-    if files:
-        print('No files found on directory')
-    else:
-        print('How many files would you like to use to train the learning algorithm? ')
-        nFiles = input()
-        while not nFiles.is_integer():
-            print('\nPlease insert a number: ')
-            nFiles = input()
+    if dirs:
+        print('Please choose a directory without any sub folders.')
+        break
+    elif files:
+        nFiles = ''
+        while not isinstance(nFiles, int):
+            try:
+                nFiles = int(input('How many files would you like to use to train the learning algorithm? '))
+            except TypeError:
+                print('That is not a valid number. \n')
+        print('Creating detector...')
         sift_detector = cv2.xfeatures2d.SIFT_create()
-        for fileName in files:
-            if fileName.lower.endswith('.jpg'):
-                img = cv2.imread(os.path.join(pathSelectionWindow.directory, fileName))
+        for fileName in sorted(files):
+            if nFiles < 1:
+                print('Ended keypoints printing...')
+                break
+            elif fileName.lower().endswith('.jpg'):
+                print('File: ' + fileName)
+                img = cv2.imread(os.path.join(root, fileName))
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                 kp = sift_detector.detect(gray, None)
-                img = cv2.drawKeypoints(gray, kp)
+                img = cv2.drawKeypoints(gray, kp, None)
                 cv2.imwrite('sift_detector_kp.jpg', img)
+                nFiles -= 1
             else:
                 print('The file: ' + fileName + ' is not a jpg image file. \n')
+    else:
+        print('No files found on directory: ' + root)
