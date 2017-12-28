f8f31aa853992551560fa2d0d9d12381901588da
Ricardo Magalhães <ricardo_magalhaes45@hotmail.com>
2017-12-28 23:48:46 +0000

    Training program with KNN, but accuracy is low

--- TP2/src/TrainingProgram.py
+++ TP2/src/TrainingProgram.py
@@ -3,9 +3,24 @@ import ObjectDefinitions
 import os
 from tkinter import filedialog
 from tkinter import *
+from imutils import paths
 from os import listdir
-from keras import Sequential
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cross_validation import train_test_split
 
+
+def image_to_feature_vector(image, size=(32, 32)):
+    return cv2.resize(image, size).flatten()
+
+
+def extract_color_histogram(image, bins=(8, 8, 8)):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hist = cv2.calcHist([hsv], [0, 1, 2], None, bins,
+                        [0, 180, 0, 256, 0, 256])
+
+    cv2.normalize(hist, hist)
+
+    return hist.flatten()
 
 # Training program to recognize various object from the AID (Aerial Image Database)
 
@@ -13,74 +28,36 @@ from keras import Sequential
 print('Waiting selection of AID...')
 rootDir = filedialog.askdirectory(title = 'Navigate to the directory containing the AID')
 
-# Ask for image size details
-imageParams = (600, 600)
-answer = ''
-while not answer.lower() == 'y' or answer.lower() == 'n':
-    answer = input('Use default image size parameters? [y/n] ')
-
-if answer.lower() == 'n':
-    width = ''
-    height = ''
-    while not (isinstance(width, int) or width < 0) :
-        if width:
-            print('Image dimensions cannot be negative. \n')
-        try:
-            width = int(input('Image width: '))
-        except TypeError:
-            print('Invalid width input. \n')
-    while not (isinstance(height, int) or height < 0 
-        if height:
-            print('Image dimensions cannot be negative. \n')
-        try:
-            height = int(input('Image height: '))
-        except TypeError:
-            print('Invalid height input. \n')
-    imageParams = (width, height)
-
-# Create keras model
-model = Sequential()
-numberOfDefinitions = len(RealWorldObjectType)
-
-
 # used to obtain all files and directories in a given root directory
 # pathSelectionInfo = walk(directory)
 
-# List all subdirectories
-subdirs = sorted(listdir(directory))
+# List all subdirectories and initialize vars
+rawImages = []
+features = []
+labels = []
+
+subdirs = sorted(listdir(rootDir))
+
 if subdirs:
     for directory in subdirs:
+        imagePaths = os.listdir(os.path.join(rootDir, directory))
+        for (i, imagePath) in enumerate(imagePaths):
+            image = cv2.imread(os.path.join(rootDir, directory, imagePath))
+            label = directory
+            pixels = image_to_feature_vector(image)
+            hist = extract_color_histogram(image)
 
+            rawImages.append(pixels)
+            features.append(hist)
+            labels.append(label)
+        print("Directory '", directory, "' loaded.")
 
-'''
-for root, dirs, files in pathSelectionInfo:
-    if dirs:
-        print('Please choose a directory without any sub folders.')
-        break
-    elif files:
-        nFiles = ''
-        while not isinstance(nFiles, int):
-            try:
-                nFiles = int(input('How many files would you like to use to train the learning algorithm? '))
-            except TypeError:
-                print('That is not a valid number. \n')
-        print('Creating detector...')
-        sift_detector = cv2.xfeatures2d.SIFT_create()
-        for fileName in sorted(files):
-            if nFiles < 1:
-                print('Ended keypoints printing...')
-                break
-            elif fileName.lower().endswith('.jpg'):
-                print('File: ' + fileName)
-                img = cv2.imread(os.path.join(root, fileName))
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                kp = sift_detector.detect(gray, None)
-                img = cv2.drawKeypoints(gray, kp, None)
-                cv2.imwrite('sift_detector_kp.jpg', img)
-                nFiles -= 1
-            else:
-                print('The file: ' + fileName + ' is not a jpg image file. \n')
-    else:
-        print('No files found on directory: ' + root)
-        break
-'''
+(trainRI, testRI, trainRL, testRL) = train_test_split(
+    rawImages, labels, test_size=0.25, random_state=42)
+(trainFeat, testFeat, trainLabels, testLabels) = train_test_split(
+    features, labels, test_size=0.25, random_state=42)
+
+model = KNeighborsClassifier(2)
+model.fit(trainRI, trainRL)
+acc = model.score(testRI, testRL)
+print("[INFO] raw pixel accuracy: {:.2f}%".format(acc * 100))
